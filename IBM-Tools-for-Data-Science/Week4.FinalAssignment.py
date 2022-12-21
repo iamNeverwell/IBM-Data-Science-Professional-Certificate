10173026a96ec1fc9890f8b19008ccfd3f10ff85
Neverwell <117388341+iamNeverwell@users.noreply.github.com>
2022-12-21 15:25:16 -0300

    Create Week4.FinalAssignment.py

---
+++ IBM-Tools-for-Data-Science/Week4.FinalAssignment.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# #### Jansen Machado
+# Mars Exploration Data Scientist
+
+# ##### Data Science allows us to better understand, and as a result, make more informed and intelligent decisions, which will accelerate our space exploration capabilities. Nothing excites and interests me more than being part of that.
+
+# ### The code below is to display a message that is within the code:
+
+# In[6]:
+
+
+print("Hello, Martians! Take me to your Leader")
+
+
+# <hr>
+# <ul>
+# <img src="https://stsci-opo.org/STScI-01EVT0YSW959M9W949RQ38DEPN.png" alt = "Hubble Telescope image of Mars">
+# <hr>
+# <center><a href="https://github.com/iamNeverwell">Click here to visit my humble Github profile</a>
+
+# In[ ]:
+
+
+
